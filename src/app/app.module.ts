53bd7378a83d41d31133f3fe2e60ca18f1918bb7
ldkien <ldkien@cmc.com.vn>
2018-03-27 17:29:42 +0700

    simple component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
+import { Wordcomponent } from './word.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Wordcomponent,
+    BookComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/word.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    templateUrl: './word.component.html',
+    selector: 'app-word',
+    styleUrls: [`./word.component.css`]
+})
+
+export class Wordcomponent{
+    en = 'Hello';
+    vn = 'Xin chao';
+}
