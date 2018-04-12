d4c89c799e08fd852e02669c355a9b5b6df58765
Le Doan Kien <kienmit95@gmail.com>
2018-04-12 22:13:46 +0700

    pipe

--- src/app/learn-pipe/learn-pipe.component.ts
+++ src/app/learn-pipe/learn-pipe.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LearnPipeComponent implements OnInit {
 
+  birthday = new Date(2015,10,4);
+  person = {name:'Ha Giang',age:'22'};
+  address = Promise.resolve('92 Le Thanh Tong');
   constructor() { }
 
   ngOnInit() {
