5bdba5d010adb6184a4dd50dd9708cd1e56305ac
Le Doan Kien <kienmit95@gmail.com>
2018-05-01 10:24:58 +0700

    tach service

--- src/app/http-service/http-service.component.ts
+++ src/app/http-service/http-service.component.ts
@@ -1,20 +1,16 @@
+import { IpService } from './../ip.service';
 import { Component, OnInit } from '@angular/core';
-import {Http} from '@angular/http';
-import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-http-service',
   templateUrl: './http-service.component.html',
-  styleUrls: ['./http-service.component.css']
+  styleUrls: ['./http-service.component.css'],
+  providers : [IpService]
 })
 export class HttpServiceComponent implements OnInit {
 
-  constructor(private http : Http) {
-    this.http.get('http://ip.jsontest.com')
-    .toPromise()
-    .then(res => res.json())
-    .then(resJson => console.log(resJson))
-    .catch(ex => console.log(ex));
+  constructor(private ipService : IpService) {
+    this.ipService.getIp().then(ip => console.log(ip)).catch(ex =>console.log(ex));
   }
 
   ngOnInit() {
